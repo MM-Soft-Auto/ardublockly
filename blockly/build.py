afe4021bfb9d4c56c19eea110d55fd67c1814949
MarekM <marek_mikolajewski@wp.pl>
2020-08-19 16:36:50 +0200

    Update for correct build of Blockly package with closure-compiler

--- blockly/build.py
+++ blockly/build.py
@@ -174,14 +174,14 @@ class Gen_compressed(threading.Thread):
     self.search_paths = search_paths
 
   def run(self):
-    self.gen_core()
+    # self.gen_core()
     self.gen_blocks()
     self.gen_generator("arduino")
-    self.gen_generator("javascript")
-    self.gen_generator("python")
-    self.gen_generator("php")
-    self.gen_generator("dart")
-    self.gen_generator("lua")
+    # self.gen_generator("javascript")
+    # self.gen_generator("python")
+    # self.gen_generator("php")
+    # self.gen_generator("dart")
+    # self.gen_generator("lua")
 
   def gen_core(self):
     target_filename = "blockly_compressed.js"
